f7e1bd8c135dbb152a8830247b814fc1886033a3
JohnnyWilkerson <149409298+JohnnyWilkerson@users.noreply.github.com>
2024-01-15 16:57:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ let ny=()=>{
 }
 
 document.getElementById("btn").addEventListener("click", ny );
+
+let ch=()=>{
+    let name = document.getElementById("npt").value;
+    document.getElementById("msg2").innerHTML= "Merry Christmas " + name + "!";
+    document.getElementById("santa").src="christmas.jpeg"
+}
+
+document.getElementById("btn2").addEventListener("click", ch );
